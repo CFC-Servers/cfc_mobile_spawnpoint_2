442be0f260b6a60110f39b1e5c75e306edefb324
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-05-29 03:01:19 +0200

    Fix error when GetClass cant get an entity

--- lua/autorun/server/sv_cfc_spawnpoint.lua
+++ lua/autorun/server/sv_cfc_spawnpoint.lua
@@ -5,11 +5,10 @@ local bannedTools = {
 }
 hook.Remove( "CanTool" , HOOK_CANTOOL )
 hook.Add( "CanTool" , HOOK_CANTOOL, function( ply, tr, tool )
-    if not tr.Entity or tr.Entity:GetClass() ~= "sent_spawnpoint" then return end
-    
+    if not tr.Hit or not tr.Entity or tr.Entity:GetClass() ~= "sent_spawnpoint" then return end
+
     if bannedTools[tool] then
         ply:ChatPrint( string.format( "You cant use '%s' on a spawnpoint", tool) )
         return false
     end
-    
 end)
