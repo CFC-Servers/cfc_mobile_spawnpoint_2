a482df2e5b7e33e0ed7d6322c50409b9234ceb89
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-05-29 03:03:47 +0200

    Apply suggestion
    Co-authored-by: Brandon Sturgeon <brandon@brandonsturgeon.com>

--- lua/autorun/server/sv_cfc_spawnpoint.lua
+++ lua/autorun/server/sv_cfc_spawnpoint.lua
@@ -5,7 +5,9 @@ local bannedTools = {
 }
 hook.Remove( "CanTool" , HOOK_CANTOOL )
 hook.Add( "CanTool" , HOOK_CANTOOL, function( ply, tr, tool )
-    if not tr.Hit or not tr.Entity or tr.Entity:GetClass() ~= "sent_spawnpoint" then return end
+    if not tr.Hit then return end
+    if not IsValid( tr.Entity ) then return end
+    if tr.Entity:GetClass() ~= "sent_spawnpoint" then return end
 
     if bannedTools[tool] then
         ply:ChatPrint( string.format( "You cant use '%s' on a spawnpoint", tool) )
